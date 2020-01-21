4c5d0963284462f13dc1e4f1a22ff2c4f1680add
Stuart Allan Rutherford <squirrel96@live.co.uk>
2020-01-21 18:24:12 +1100

    Add dynamaxing

--- rockslidebot.py
+++ rockslidebot.py
@@ -7,10 +7,17 @@ token = os.getenv('DISCORD_TOKEN')
 
 class CustomClient(discord.Client):
   async def on_message(self, message):
-      if self.user.id != message.author.id:
-          if random.randrange(10) < 3:
-              await message.delete()
-              await message.channel.send(message.author.mention + ' flinched and couldn\'t post!')
+      if self.user.id == message.author.id:
+          return
+      if "dynamax" in [role.name.lower() for role in message.author.roles]:
+          print('The flinch was blocked by the power of Dynamax!')
+          return
+      if random.randrange(10) >= 3:
+          print('> tfw you dont get the hax')
+          return
+      print('FLINCH!')
+      await message.delete()
+      await message.channel.send(message.author.mention + ' flinched and couldn\'t post!')
 
 client = CustomClient()
 client.run(token)
